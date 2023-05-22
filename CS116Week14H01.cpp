a22e117aebe9d1075b343d801a931801989bff2e
scg990129@yahoo.com.hk <scg990129@yahoo.com.hk>
2023-05-22 00:24:11 -0700

    Add project files.

---
+++ CS116Week14H01.cpp
@@ -0,0 +1,21 @@
+//CS116Week14H01
+//20230516
+//Shun Hoi Yeung
+//900494756
+
+// This program shows an array name being dereferenced with the 
+// operator.
+
+#include <iostream>
+// #include <iomanip>
+// #include <vector>
+
+using namespace std;
+
+int main() {
+    short numbers[] = { 10, 20, 30, 40, 50 };
+    cout << "The first element of the array is ";
+
+    cout << *numbers << endl;
+    return 0;
+}
